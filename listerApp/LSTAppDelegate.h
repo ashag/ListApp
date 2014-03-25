1e64efeabc7751d707b46f9bd3270194a2dd8e98
Agolveo <agolveo@gmail.com>
2014-03-25 11:32:17 -0700

    Initial Commit

---
+++ listerApp/LSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LSTAppDelegate.h
+//  listerApp
+//
+//  Created by Asha Golveo on 3/25/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ listerApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  listerApp
+//
+//  Created by Asha Golveo on 3/25/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LSTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LSTAppDelegate class]));
+    }
+}
